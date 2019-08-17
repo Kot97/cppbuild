949a0051e564c11b8d829b1bef0255ca1b9b6f4f
Kot97 <marcin.kurdziel97@gmail.com>
2019-08-17 16:12:30 +0200

    add nobuild option

--- alpine-clang/build.sh
+++ alpine-clang/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [ ! -d /$1/$buildname ]; then
-    mkdir /$1/$buildname
+if [ $1 = "nobuild" ]; then
+    sh -i
+else
+    if [ ! -d /$1/$buildname ]; then
+        mkdir /$1/$buildname
+    fi
+    cd /$1/$buildname && cmake .. && make
 fi
-
-cd /$1/$buildname && cmake .. && make
