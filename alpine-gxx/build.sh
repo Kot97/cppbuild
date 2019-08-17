3d44f84016b7b9fad86491ab1718f457f63663ad
Kot97 <marcin.kurdziel97@gmail.com>
2019-08-17 23:51:24 +0200

    separate gcc from gxx

---
+++ alpine-gxx/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+
+if [ $1 = "nobuild" ]; then
+    sh -i
+else
+    if [ ! -d /$1/$buildname ]; then
+        mkdir /$1/$buildname
+    fi
+    cd /$1/$buildname && cmake .. && make
+
+    if [ ! -z $2 ] && [ $2 = "noexit" ]; then
+        sh -i
+    fi
+fi
